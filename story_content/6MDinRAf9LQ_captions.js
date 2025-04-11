56facf83415ab4761ee24517cc0917bfa823e20f
ramonanicole <112777685+ramonanicole@users.noreply.github.com>
2025-04-11 16:04:04 -0500

    test

--- story_content/6MDinRAf9LQ_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.96.33815.0%0D%0A%0D%0A00:00:00.100%20--%3E%2000:00:00.971%0D%0A[slurping%20up%20soup]%0D%0A%0D%0A"}')
