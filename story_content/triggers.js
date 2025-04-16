55f98a4cfb1c4e77c5b7bc9ec83aa9623a9cd074
ramonanicole <112777685+ramonanicole@users.noreply.github.com>
2025-04-15 22:25:01 -0500

    update

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5gDAypQuyFR":
+      case "6B80nSEMaYv":
         Script1();
         break;
   }
@@ -16,3 +16,12 @@ var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
 };
+function getActor() {
+  return {
+    "objectType": "Agent",
+    "account": {
+      "homePage": "https://www.example.com",
+      "name": "Random" + Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+    }
+  };
+}
